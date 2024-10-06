352d64301c3c8cf1bf0f4f81f2e3c24c3124e13f
safetrix <127260443+safetrix@users.noreply.github.com>
2024-10-06 16:36:19 -0600

    Update client.py

--- client.py
+++ client.py
@@ -1,22 +1,26 @@
+
 import socket
+import logging
 
-
-
+logging.basicConfig(filename = 'client_connections.log', level = logging.INFO)
 def start_connection(host, port):
     addr = (host, port)
 
     try:
         print("starting connection to", addr)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        logging.info(f"connection created: {addr}")
         sock.setblocking(True)
         sock.connect((host,port))
-        sock.send(b"test")
     
-       
         message = sock.recv(1024).decode()
-        print("got back message: ", message)
+        print(message)
+        sock.send(b"ready")
+
     except Exception as e:
         print("Error: ", e)
+        logging.error("Error in connection attempt: ", e)
+
 #events = selectors.EVENT_READ | selectors.EVENT_WRITE
 #message = libclient.Message(sel, sock, addr, request)
 #sel.register(sock, events, data=message)
