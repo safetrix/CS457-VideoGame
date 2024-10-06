6d5dc6d12d7077e8abbc2e907fe8d9d9c7894fcf
safetrix <127260443+safetrix@users.noreply.github.com>
2024-10-06 12:11:08 -0600

    Update server.py

--- server.py
+++ server.py
@@ -3,9 +3,10 @@ import socket
 import selectors
 import traceback
 import threading
+import logging
 
-serverSelector = selectors.DefaultSelector()
-
+#serverSelector = selectors.DefaultSelector()
+logging.basicConfig(filename = 'connections.log', level = logging.INFO)
 
 
 def start():
@@ -16,21 +17,32 @@ def start():
 
     print("listening on 127.0.0.1:65432")
 
+ 
     clients = 0 # need to keep track of total clients
     while clients < 2:
-        clientSocket, addr = lsock.accept()
-        print("accepted a connection from client: ", addr)
-        clients += 1
-
+        try:
+            clientSocket, addr = lsock.accept()
+            logging.info(f"connection created: {clientSocket}")
+            print("accepted a connection from client: ", addr)
+            clients += 1
+        
         # OPTIONAL, not sure if we need to multithread, but we can do it for now
 
-        client_thread = threading.Thread(target=client_response, args=(clientSocket,addr))
-        client_thread.start()
+            client_thread = threading.Thread(target=client_response, args=(clientSocket,addr))
+            client_thread.start()
+        except Exception as e:
+            logging.error("Error in connection attempt: ", e)
 
 def client_response(clientSocket, addr):
     while True:
         message = clientSocket.recv(1024) # need to adjust size based on implementation
+
+        if not message:
+            break
         print("Recieved a message from ", addr)
         print("message recieved: ", message.decode())
+        print("sending messages back to client")
+        clientSocket.send(b"ACK")
+        
 
 start()
